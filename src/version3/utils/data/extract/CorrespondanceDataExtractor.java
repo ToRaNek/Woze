57ada5f3fbc6f7771fcca6cbc97bd0659c3aca77
GASPARD CATRY <gaspard.catry.etu@univ-lille.fr>
2024-06-15 00:25:08 +0200

    Ajout d'une classe CorrespondanceDataExtractor.

---
+++ src/version3/utils/data/extract/CorrespondanceDataExtractor.java
@@ -0,0 +1,8 @@
+package version3.utils.data.extract;
+
+public class CorrespondanceDataExtractor {
+
+    private static final String path_cor = "res/version3/data/correspondances.csv";
+
+    public static String[] data_cor = DataExtractor.listeData(path_cor);
+}
